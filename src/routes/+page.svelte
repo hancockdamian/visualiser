83bd9db1cd5cf3b571fdb549ac9b0691bf08945e
hancockdamian <damian.hancock09@gmail.com>
2025-02-23 08:23:40 +0000

    feat: bring in webgl and threejs to do some particle sim fun

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,9 @@
 <script>
+    import "../styles/global.css"
     import Visualiser from '../components/Visualiser.svelte';
 </script>
 
 <main>
-    <h1>Visualiser Page</h1>
+    <h1>test site</h1>
     <Visualiser />
 </main>
-
-<style>
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 600px;
-        margin: auto;
-    }
-</style>
